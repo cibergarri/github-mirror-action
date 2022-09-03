10d101e5be700987cce1be3ab3b448ab480b3368
n14s <n14x@posteo.de>
2022-09-03 18:01:54 +0200

    add logging

--- action.sh
+++ action.sh
@@ -7,8 +7,21 @@ if [ -z "${REMOTE}" ]; then
   exit 1
 fi
 
+ls -la
+git status
 git clone --bare "https://${GITHUB_ACTOR}:${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git" . || exit 1
+ls -la
+git status
 git config --global --add safe.directory /github/workspace
+ls -la
+git status
 git remote add --mirror=fetch mirror "${REMOTE}" || exit 1
+ls -la
+git status
 git fetch mirror +refs/heads/*:refs/remotes/origin/* || exit 1
+ls -la
+git status
 git push --force --mirror --prune origin || exit 1
+ls -la
+git status
+
