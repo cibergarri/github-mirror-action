7b921eac7b26ce1dfb571b23d881dccdcabcadaf
cibergarri <cibergarri@hotmail.com>
2024-10-03 13:54:41 +0200

    feat: add branches filter

--- action.sh
+++ action.sh
@@ -2,6 +2,9 @@
 
 REMOTE="${1}"
 
+ORIGIN_BRANCH="${2}"
+DESTINATION_BRANCH="${3}" || "${ORIGIN_BRANCH}"
+
 if [ -z "${REMOTE}" ]; then
   echo Please specify an origin
   exit 1
@@ -13,9 +16,24 @@ git clone --bare "https://${GITHUB_ACTOR}:${GITHUB_TOKEN}@github.com/${GITHUB_RE
 ls -la
 git status
 git config --global --add safe.directory /github/workspace
+
 ls -la
 git status
 git remote add --mirror=fetch mirror "${REMOTE}" || exit 1
+
+if [ -z "${ORIGIN_BRANCH}" ]; then
+  ls -la
+  git status
+  git fetch mirror +refs/heads/*:refs/remotes/origin/* || exit 1
+  git fetch mirror ${ORIGIN_BRANCH} ${DESTINATION_BRANCH}
+
+  ls -la
+  git status
+  git push --force --prune origin ${DESTINATION_BRANCH} || exit 1
+  exit 1
+fi
+
+
 ls -la
 git status
 git fetch mirror +refs/heads/*:refs/remotes/origin/* || exit 1
