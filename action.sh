e56baa5944f49d53016457d84430191fa351cf75
cibergarri <cibergarri@hotmail.com>
2024-10-03 14:19:13 +0200

    chore: add logs

--- action.sh
+++ action.sh
@@ -5,6 +5,11 @@ REMOTE="${1}"
 ORIGIN_BRANCH="${2}"
 DESTINATION_BRANCH="${3}" || "${ORIGIN_BRANCH}"
 
+echo "REMOTE: ${REMOTE}"
+echo "ORIGIN_BRANCH: ${ORIGIN_BRANCH}"
+echo "DESTINATION_BRANCH: ${DESTINATION_BRANCH}"
+echo "GITHUB: https://${GITHUB_ACTOR}:<pass>@github.com/${GITHUB_REPOSITORY}.git"
+
 if [ -z "${REMOTE}" ]; then
   echo Please specify an origin
   exit 1
